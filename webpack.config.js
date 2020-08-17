f6c09b6640bf0247374635d290210d69a1311732
gabriel carvajal pizarro <g.carvajal.pizarro@gmail.com>
2020-08-17 08:41:11 -0400

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,35 @@ const path = require(`path`);
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
 const webpack = require("webpack"); //to access built-in plugins
 
+let mode = `development`;
+
+let entries = {
+  btn: `./src/js/like_button.js`,
+};
+
 module.exports = {
-  entry: "./public_html/src/js/like_button.js",
-  output: {
-    //publicPath: "/public_html",
+  mode: mode,
+  entry: entries,
+  plugins: [
+    //Añadir un HtmlWebpackPlugin por cada entry
+    new HtmlWebpackPlugin({
+      filename: `index.html`,
+      template: `src/pages/index.html`,
+      chunks: [`btn`],
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+  ],
+
+  /*output: {
+    publicPath: "/public_html",
     path: path.resolve(__dirname + "/public_html", "dist"),
     filename: "bundle.js",
+  },*/
+  output: {
+    filename: `js/[name].[contenthash].js`,
+    //Designa un directorio publico para los archivos. Puede generar problemas ya que es absoluto
+    //publicPath: "/public_html",
+    path: __dirname + `/public_html`,
+    library: `[name]`,
   },
 };
