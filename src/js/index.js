39ff548a0383cfed1359b1456f6e185edd966a71
gcarvajalp <g.carvajal.pizarro@gmail.com>
2020-09-07 11:30:53 -0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,5 @@ import ReactDOM from "react-dom";
 import ClickButton from "../components/ClickButton";
 import Counter from "../components/Counter";
 
-//ReactDOM.render(<ClickButton />, document.getElementById("container"));
-ReactDOM.render(<Counter />, document.getElementById("container"));
+ReactDOM.render(<ClickButton />, document.getElementById("container"));
+//ReactDOM.render(<Counter />, document.getElementById("container"));
