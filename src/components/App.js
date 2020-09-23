9ca4af6f28d8f89ebe7c418bb857d6a60ae80248
gabriel <g.carvajal.pizarro@gmail.com>
2020-09-22 21:25:14 -0300

    added example about useReduccer

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 const INITIAL_LIST = [
@@ -8,9 +8,21 @@ const INITIAL_LIST = [
   }
 ];
 
+const listReducer = (state, action)=>{
+  switch(action.type){
+    case 'ADD_ITEM':
+      return state.concat({name: action.name, id: action.id});
+    default:
+      throw new Error();
+  }
+};
+
 function App(){
   const [name,setName] = useState('');
-  const [list, setList] = useState();
+  //const [list, setList] = useState();
+  
+  const [list,dispatchList] = useReducer(listReducer,INITIAL_LIST);
+
 
   function handleChange(event){
     console.log('handleChange');
@@ -18,17 +30,9 @@ function App(){
   }
 
   function handleAdd(){
-    
-    let newList;
-
-    if(list){
-      newList = list.concat({id: uuidv4(), name: name});
-    }else{
-      newList = [{id: uuidv4(), name: name}];
-    }
-    
-    setList(newList);
+    dispatchList({type: 'ADD_ITEM', name: name, id : uuidv4()});
     setName('');
+    
   }
 
   return(
